650b8d0e649fdf57ea1dc0a23da995e130258d46
Aleksandr <saniatagilov@gmail.com>
2020-05-23 21:24:25 +0300

    #20 edit assign

--- backend/todos/models.py
+++ backend/todos/models.py
@@ -36,7 +36,7 @@ class Table(models.Model):
 
 class Card(models.Model):
     title = models.CharField(max_length=30, verbose_name='desk title')
-    assignees = models.ManyToManyField('customers.Customer')
+    assignees = models.ManyToManyField(Customer)
     date_expiration = models.DateField(verbose_name='date expiration')
     is_done = models.BooleanField(verbose_name='done flag')
     table = models.ForeignKey(
