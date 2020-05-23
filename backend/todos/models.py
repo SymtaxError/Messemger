7a208d0ad2dc1205318d87c4547919f16fecb681
Aleksandr <saniatagilov@gmail.com>
2020-05-23 20:24:35 +0300

    #20 init Card

--- backend/todos/models.py
+++ backend/todos/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Card(models.Model):
+    title = models.CharField(max_length=30, verbose_name='desk title')
+    assignees = models.ManyToManyField('customers.Customer')
+    date_expiration = models.DateField(verbose_name='date expiration')
+    is_done = models.BooleanField(verbose_name='done flag')
