fccf88a38e254e76f1f05872835fb2fd4a5e7ea2
Muravev Ilia <degtyarev327@gmail.com>
2020-05-24 13:53:59 +0300

    #13 add registration

--- backend/customers/views.py
+++ backend/customers/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import CustomerSerializer
 
-# Create your views here.
+class CustomerRegistrationView(APIView):
+    def post(self, request):
+        customer = CustomerSerializer(data=request.data)
+        if customer.is_valid():
+            customer.save()
+            return Response(status=status.HTTP_201_CREATED)
+        return Response(customer.errors, status=status.HTTP_400_BAD_REQUEST)
