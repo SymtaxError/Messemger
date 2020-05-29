c5f4f560d0c7f6c0aff0072b5b48258c0c04a228
Muravev Ilia <degtyarev327@gmail.com>
2020-05-29 20:08:02 +0300

    #23 add profile serializer

--- backend/customers/serializers.py
+++ backend/customers/serializers.py
@@ -5,3 +5,8 @@ class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Customer
         fields = ['email', 'password', 'first_name', 'last_name']
+
+class ProfileSerializer(CustomerSerializer):
+    class Meta:
+        model = Customer
+        fields = ['email', 'first_name', 'last_name']
