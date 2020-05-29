159b73a0bd6c840eeaaa7073f6b74cb534b733be
shre19u708 <kluskape@gmail.com>
2020-05-29 03:29:23 +0300

    First version of Login/Register requests #15

--- frontend/src/api/http.ts
+++ frontend/src/api/http.ts
@@ -1,4 +1,8 @@
-export const backendURL = "";
+import {UserUnit} from "api/models/user";
+import {stringify} from "querystring";
+import {Ref} from "react";
+
+export const backendURL = "localhost:8000/";
 
 const composeArgs = (args: Record<string, string>): string => {
     const entries = Object.entries(args);
@@ -8,9 +12,35 @@ const composeArgs = (args: Record<string, string>): string => {
 
 const headers = {
     "Content-Type": "application/json; charset=UTF-8",
-    "Authorization": (localStorage.getItem("token") !== "undefined")
-        ? `JWT-token ${localStorage.getItem("token")}`
+    "access": (localStorage.getItem("access") !== "undefined")
+        ? `${localStorage.getItem("access")}`
         : ""
+};
+
+interface RefreshUnit {
+    access: string
+    refresh: string
+}
+
+const resultRefresh = async (): Promise<void> => {
+    const bodyForCheck = {
+        "refresh": (localStorage.getItem("refresh") !== "undefined")
+            ? `${localStorage.getItem("refresh")}`
+            : ""
+    };
+    const response = await fetch(
+        `${backendURL}/auth/jwt/refresh/`, // Сюда адрес для проверки временного токена
+        {method: "POST", body: JSON.stringify(bodyForCheck)}
+    );
+    const state = response.status;
+    const result: RefreshUnit = JSON.parse(await response.text());
+    if (state === 200) {
+        localStorage.setItem("access", result.access);
+        localStorage.setItem("refresh", result.refresh);
+    } else if (state === 401) {
+        window.location.replace("localhost:3000/login")
+    }
+    return;
 };
 
 const request = async <T>(
@@ -26,13 +56,9 @@ const request = async <T>(
     );
 
     if (result.status === 403) {
-        alert("Доступ запрещен");
-    }
-    if (result.status === 401) {
-        const msg = (await result.text() === "denied")
-            ? "Неправильный логин или пароль"
-            : "Вы не авторизованы";
-        alert(msg);
+        resultRefresh();
+    } else if (result.status === 401) {
+        alert(result.text());
     }
     return JSON.parse(await result.text() || "null") as unknown as T;
 };
@@ -47,4 +73,16 @@ export const http = {
     delete: async <T>(path: string, args: Record<string, string>): Promise<T> => {
         return request(path, args, "DELETE");
     }
+};
+
+export const registerRequest = async (user: UserUnit): Promise<void> => {
+    const args = {};
+    await http.post("/users/register/", args, JSON.stringify(user));
+    return;
+};
+
+export const loginRequest = async (email: string, password: string): Promise<void> => {
+    const args = {};
+    await http.post("/auth/jwt/login/", args, JSON.stringify({"email": email, "password": password}));
+    return;
 };
---
+++ frontend/src/api/models/user.ts
@@ -0,0 +1,6 @@
+export interface UserUnit {
+    email: string
+    password: string
+    first_name: string
+    last_name: string
+}
