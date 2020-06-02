842cf4b65363a04e414c394fd71f832ba6050d78
Serova Maria <mariya.serova2010@yandex.ru>
2020-06-02 22:43:55 +0300

    #37 Added NewsPostView and url for news

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
 import authentication.urls, users.urls, servers.urls
+from news.views import NewsPostView
 
 urlpatterns = [
     path('auth/', include(authentication.urls)),
     path('users/', include(users.urls)),
-    path('servers/', include(servers.urls))
+    path('servers/', include(servers.urls)),
+    path('news/', NewsPostView.as_view()),
 ]
--- backend/news/views.py
+++ backend/news/views.py
@@ -1,3 +1,77 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status, permissions
+from .serializers import NewsPostSerializer
+from .models import NewsPost
 
-# Create your views here.
+class NewsPostView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+    
+    def get(self, request):
+        count = int(request.GET.get('count'))
+        start = int(request.GET.get('start')) - 1
+        try:
+            query_set = NewsPost.objects.all()[start : start + count]
+            print(NewsPost.objects.all()[start : start + count])
+            serializer = NewsPostSerializer(
+                query_set, 
+                many=True
+            )
+            return Response(
+                serializer.data,
+                status=status.HTTP_200_OK
+            )
+        except:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+    
+    def post(self, request):
+        user = request.user
+        print(user.is_superuser)
+        if user.is_superuser:
+            query_set = {}
+            for item in request.data.items():
+                query_set[item[0]] = item[1]
+            query_set['author'] = str(user)
+            serializer = NewsPostSerializer(data=query_set)
+            if serializer.is_valid():
+                data = serializer.validated_data
+                NewsPost.objects.create_news_post(
+                    data['title'], 
+                    data['text'],
+                    user
+                )
+                return Response(status=status.HTTP_201_CREATED)
+            else:
+                return Response(
+                    serializer.errors,
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+        else:
+            return Response(status=status.HTTP_403_FORBIDDEN)
+
+    def delete(self, request):
+        post_id = int(request.GET.get('post_id'))
+        if request.user.is_superuser:
+            NewsPost.objects.get(id=post_id).delete()
+            return Response(status=status.HTTP_200_OK)
+        return Response(status=status.HTTP_403_FORBIDDEN)
+
+    def put(self, request):
+        post_id = int(request.GET.get('post_id'))
+        if request.user.is_superuser:
+            query_set = {}
+            for item in request.data.items():
+                query_set[item[0]] = item[1]
+            query_set['author'] = str(request.user)
+            serializer = NewsPostSerializer(data=query_set)
+            if serializer.is_valid():
+                post = NewsPost.objects.get(id=post_id)
+                post.update(**serializer.validated_data)
+                return Response(status=status.HTTP_200_OK)
+            else:
+                return Response(
+                    serializer.errors,
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+        return Response(status=status.HTTP_403_FORBIDDEN)
