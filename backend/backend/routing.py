227b141fce50f8bb9f5e98a36f1c23a75c32c110
Muravev Ilia <degtyarev327@gmail.com>
2020-06-03 14:03:35 +0300

    separate todos and chats consumers and routing, makemigrations for todos

--- backend/backend/routing.py
+++ backend/backend/routing.py
@@ -1,12 +1,13 @@
 from channels.routing import ProtocolTypeRouter, URLRouter
 from authentication.middleware import JWTAuthMiddlewareStack
-import servers.routing
+import servers.routing, todos.routing
 from django.urls import include, path
 
 application = ProtocolTypeRouter({
     'websocket': JWTAuthMiddlewareStack(
         URLRouter([
             path('', servers.routing.websocket_urlpatterns),
+            path('', todos.routing.websocket_urlpatterns)
         ]),
     )
 })
