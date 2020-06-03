6e27cd3e0ed926d55c1b15d53c6e7d21ec509db0
Aleksandr <saniatagilov@gmail.com>
2020-06-03 21:27:43 +0300

    #34 add creation multiple users

--- backend/todos/views.py
+++ backend/todos/views.py
@@ -5,10 +5,13 @@ from rest_framework import status, permissions
 from .serializers import DeskSerializer
 from .models import Desk
 import todos.methods
+from users.models import UserProfile
+import json
+
+
 # from .todos.methods import
 # from users.models import UserProfile
 # import todos.methods
-
 
 
 class DeskView(APIView):
@@ -16,7 +19,6 @@ class DeskView(APIView):
 
     def get(self, request):
         user = request.user
-        print(request)
         desks = DeskSerializer(user.desk_set.all(), many=True)
         return Response(desks.data, status=status.HTTP_200_OK)
 
@@ -26,13 +28,15 @@ class DeskView(APIView):
             data = serializer.validated_data
             user = request.user
             if (data['server'] in [server for server in user.server_set.all()]):
-                # user = UserProfile.objects.get(tag=data['tag']).user
                 desk = Desk.objects.create_desk(title=data['title'],
-                                                  server=data['server'],
-                                                  creator=user)
+                                                server=data['server'],
+                                                creator=user)
+                if 'tags' in request.data.keys():
+                    for nickname in json.loads(request.data['tags']):
+                        user = UserProfile.objects.get(tag=nickname).user
+                        desk.users.add(user)
                 desk.save()
                 return Response(status=status.HTTP_201_CREATED)
-                # pass
             return Response(serializer.errors, status=status.HTTP_403_FORBIDDEN)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
@@ -45,14 +49,9 @@ class DeskView(APIView):
                 text_data[item[0]] = item[1]
             if not 'title' in text_data.keys():
                 return Response(status=status.HTTP_400_BAD_REQUEST)
-            # serializer = DeskSerializer(data={**text_data})
-            # print('aaa')
-            # if serializer.is_valid():
             desk = Desk.objects.get(id=id)
             desk.edit_title(text_data['title'])
-
             desk.save()
             return Response(status=status.HTTP_200_OK)
         else:
-            # else:
             return Response(status=status.HTTP_406_NOT_ACCEPTABLE)
