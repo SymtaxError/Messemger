27f05a1157362d7bd8e3f6fda9daf11527106894
Aleksandr <saniatagilov@gmail.com>
2020-06-03 20:59:55 +0300

    #34 add rename&creation

--- backend/todos/models.py
+++ backend/todos/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 from servers.models import Server, Label
 from users.models import User
+from .managers import DeskManager
 
 class Desk(models.Model):
     id = models.AutoField(primary_key=True, unique=True)
     title = models.CharField(max_length=30, verbose_name='desk title')
-    users = models.ManyToManyField(User, verbose_name='users')
     server = models.ForeignKey(
         Server,
         verbose_name="server",
@@ -16,6 +16,11 @@ class Desk(models.Model):
         related_name='desk_creator',
         on_delete=models.CASCADE
     )
+    objects = DeskManager()
+    users = models.ManyToManyField(User, verbose_name='users')
+    def edit_title(self, new_title):
+        self.__dict__['title'] = new_title
+        self.save()
 
 class Table(models.Model):
     id = models.AutoField(primary_key=True, unique=True)
--- backend/todos/views.py
+++ backend/todos/views.py
@@ -4,7 +4,11 @@ from rest_framework.response import Response
 from rest_framework import status, permissions
 from .serializers import DeskSerializer
 from .models import Desk
-import servers.methods
+import todos.methods
+# from .todos.methods import
+# from users.models import UserProfile
+# import todos.methods
+
 
 
 class DeskView(APIView):
@@ -12,21 +16,46 @@ class DeskView(APIView):
 
     def get(self, request):
         user = request.user
+        print(request)
         desks = DeskSerializer(user.desk_set.all(), many=True)
         return Response(desks.data, status=status.HTTP_200_OK)
 
-    def post(self, request, *args):
+    def post(self, request):
         serializer = DeskSerializer(data=request.data)
         if serializer.is_valid():
             data = serializer.validated_data
             user = request.user
-            # print(data['server'] in [server for server in user.server_set.all()])
             if (data['server'] in [server for server in user.server_set.all()]):
-                # add desk
-                # TODO
-
+                # user = UserProfile.objects.get(tag=data['tag']).user
+                desk = Desk.objects.create_desk(title=data['title'],
+                                                  server=data['server'],
+                                                  creator=user)
+                desk.save()
                 return Response(status=status.HTTP_201_CREATED)
                 # pass
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response(serializer.errors, status=status.HTTP_403_FORBIDDEN)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request):
+        id = int(request.GET.get('id'))
+        if todos.methods.desk_has_user(request, id):
+            text_data = {}
+            for item in request.data.items():
+                text_data[item[0]] = item[1]
+            if not 'title' in text_data.keys():
+                return Response(status=status.HTTP_400_BAD_REQUEST)
+            print(text_data)
+            # serializer = DeskSerializer(data={**text_data})
+            # print('aaa')
+            # if serializer.is_valid():
+            desk = Desk.objects.get(id=id)
+            print(desk.title)
+            desk.edit_title(text_data['title'])
+            print(desk.title)
+
+            desk.save()
+            return Response(status=status.HTTP_200_OK)
+        else:
+            # else:
+            return Response(status=status.HTTP_406_NOT_ACCEPTABLE)
