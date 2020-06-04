c273d79ccd546de20c267195785e4d6a968d4a55
shre19u708 <kluskape@gmail.com>
2020-06-04 15:28:41 +0300

    lets continue chats development

--- frontend/src/api/http.ts
+++ frontend/src/api/http.ts
@@ -129,7 +129,7 @@ export const getChatList = async (): Promise<ChatType[]> => {
 };
 
 export const getMessagesRequest = async (id: number): Promise<MessageType[]> => {
-    const args = {"chat_id": `${id}`, "count": "20", "start": "0"};
+    const args = {"chat_id": `${id}`, "count": "20", "start": "1"};
     const response = await http.get("/servers/messages/", args);
     return response.body as unknown as MessageType[]
 };
