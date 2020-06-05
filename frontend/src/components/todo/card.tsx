3b171da071111a15b4c2f56944629bc2f87532bd
shre19u708 <kluskape@gmail.com>
2020-06-05 22:48:33 +0300

    Todo

--- frontend/src/components/todo/card.tsx
+++ frontend/src/components/todo/card.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import styles from "components/todo/card.module.css";
 import doneImg from "../../img/done.png";
-import notdoneImg from "img/notdone.png";
 import deleteImg from "img/delete.png";
+import {CardType} from "../../api/models/todoModel";
 
-export const Card: React.FC = unit => {
+interface CardProps {
+    unit: CardType
+}
+
+export const Card: React.FC<CardProps> = x => {
 
     return (
         <div className={styles.card}>
             <div className={styles.done}>
                 <img src={doneImg} className={styles.doneImg} alt={""}/>
             </div>
-            <div className={styles.task}>{unit}</div>
+            <div className={styles.task}>{x.unit.cardTitle}</div>
             <div className={styles.delete}>
                 <img src={deleteImg} className={styles.deleteImg} alt={""}/>
             </div>
--- frontend/src/components/todo/column.tsx
+++ frontend/src/components/todo/column.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from "components/todo/column.module.css";
 import addImg from "img/add.png";
 import {ColumnType} from "api/models/todoModel";
+import {Card} from "./card";
 
 interface ColumnProps {
     unit: ColumnType
@@ -12,6 +13,9 @@ export const Column: React.FC<ColumnProps> = x => {
     return (
         <div className={styles.column}>
             <div className={styles.headOfColumn}>{x.unit.columnTitle}</div>
+            {
+                x.unit.cards.map((unit, key) => <Card unit={unit} key={`Card-unit-${key}`}/>)
+            }
             <div className={styles.addCard}>
                 <img src={addImg} className={styles.addImg} alt={""}/>
             </div>
