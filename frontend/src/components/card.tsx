e9700e27adc0d89b2d55005d8aa009524e143edb
Grakholskaya Yuliya <grakhjulia@mail.ru>
2020-06-05 18:17:29 +0300

    components were added

---
+++ frontend/src/components/card.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import styles from "components/card.module.css";
+import doneImg from "../img/done.png";
+import notdoneImg from "img/notdone.png";
+import deleteImg from "img/delete.png";
+
+export const RightBar: React.FC = () => {
+
+    return (
+        <div className={styles.card}>
+            <div className={styles.done}>
+                <img src={doneImg} className={styles.doneImg} alt={""}/>
+            </div>
+            <div className={styles.task}>Задача 1</div>
+            <div className={styles.delete}>
+                <img src={deleteImg} className={styles.deleteImg} alt={""}/>
+            </div>
+        </div>
+    )
+};
