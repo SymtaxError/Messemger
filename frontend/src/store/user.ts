cafbe1830d2d94ba7f7dc16c8e4056ba169c2b4a
shre19u708 <kluskape@gmail.com>
2020-06-07 00:56:32 +0300

    Fix

--- frontend/src/store/user.ts
+++ frontend/src/store/user.ts
@@ -1,5 +1,38 @@
 import {createStore, Store, Event, createEvent, createEffect, Effect} from "effector";
 import {UserUnit} from "api/models/user";
+import {backendURL} from "../api/http";
+
+interface RefreshUnit {
+    access: string
+    refresh: string
+}
+
+const resultRefresh = async (): Promise<void> => {
+    const bodyForCheck = {
+        "refresh": (localStorage.getItem("refresh") !== "undefined")
+            ? `${localStorage.getItem("refresh")}`
+            : ""
+    };
+    const headers = {
+        "Content-Type": "application/json; charset=UTF-8"
+    };
+    const response = await fetch(
+        `${backendURL}/auth/jwt/refresh/`, // Сюда адрес для проверки временного токена
+        {method: "POST", body: JSON.stringify(bodyForCheck), mode: "cors", headers: headers}
+    );
+    const state = response.status;
+    const result: RefreshUnit = JSON.parse(await response.text());
+    if (state === 200) {
+        localStorage.setItem("access", result.access);
+        localStorage.setItem("refresh", result.refresh);
+        await UserStore.getUser()
+
+    } else {
+        console.log("want redirect");
+        // window.location.replace("localhost:3000/login");
+    }
+    return;
+};
 
 export const userDataRequest = async (): Promise<UserUnit> => {
     const headers = {
@@ -12,6 +45,8 @@ export const userDataRequest = async (): Promise<UserUnit> => {
         `http://localhost:8000/users/profile/`,
         {method: "GET", headers: headers, mode: "cors"}
     );
+    if (response.status === 401)
+        await resultRefresh();
     return JSON.parse(await response.text()) as unknown as UserUnit;
 };
 
