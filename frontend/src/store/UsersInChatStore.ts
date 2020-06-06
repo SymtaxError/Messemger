37d0e94286b63a30c081c9543e902a1721662e96
shre19u708 <kluskape@gmail.com>
2020-06-07 00:34:45 +0300

    List of Users

---
+++ frontend/src/store/UsersInChatStore.ts
@@ -0,0 +1,40 @@
+import {UserUnit} from "api/models/user";
+import {createStore, Store, Event, createEvent, createEffect, Effect} from "effector";
+import {getUsersInChatRequest} from "../api/http";
+
+interface UsersInChat {
+    users: UserUnit[]
+}
+
+const initialData: UsersInChat = {
+    users: []
+};
+
+interface UsersInChatStore extends Store<UsersInChat> {
+    setUsers: Event<UserUnit[]>
+    getUsers: Effect<number, UserUnit[]>
+}
+
+export const UsersInChatStore = (() => {
+    const store = createStore<UsersInChat>(initialData) as UsersInChatStore;
+
+    store.setUsers = createEvent<UserUnit[]>("save all users");
+    store.on(store.setUsers, ((state, payload) => ({ ...state, user: payload })));
+
+    store.getUsers = createEffect({
+        name: "getUser",
+        handler: async (id: number): Promise<UserUnit[]> => {
+            const response = await getUsersInChatRequest(id);
+            console.log("Users List: ", response);
+            return response as unknown as Promise<UserUnit[]>;
+        }
+    });
+    store.getUsers.fail.watch(({ error }) => {
+        console.error("Bad credentials", error);
+        return;
+    });
+    store.getUsers.done.watch(result => {
+        store.setUsers(result.result);
+    });
+    return store;
+})();
