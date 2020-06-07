ef05d70a9c2451eb7df76e28247177b51e93bed1
Grakholskaya Yuliya <grakhjulia@mail.ru>
2020-06-07 14:26:51 +0300

    components of users

--- frontend/src/components/rightBar.tsx
+++ frontend/src/components/rightBar.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import styles from "components/rightBar.module.css";
 import {UserUnit} from "../api/models/user";
+import usersImg from "img/users.png"
+import plusImg from "img/plus.png";
+import {User} from "../components/user";
 
 interface RightBarProps {
     users: UserUnit[]
@@ -19,11 +22,19 @@ const RightBarElement: React.FC<RightBarElementProps> = x => {
     )
 };
 
+
 export const RightBar: React.FC<RightBarProps> = x => {
 
+    const count = 4;
     return (
         <div className={styles.rightBar}>
             {x.users.map((unit, key) => <RightBarElement user={unit} key={`message-unit-${key}`}/>)}
+            <div className={styles.header}>
+                <img src={usersImg} className={styles.usersImg} alt={""}/>
+                <div className={styles.members}>{`Участники(${count})`}</div>
+                <img src={plusImg} className={styles.plusImg} alt={""}/>
+            </div>
+            <User/>
         </div>
     )
 };
