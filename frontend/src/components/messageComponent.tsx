7e2cb345a1cec5fad8f9c86c8cab4a6547630333
shre19u708 <kluskape@gmail.com>
2020-06-07 12:08:55 +0300

    Dates

--- frontend/src/components/messageComponent.tsx
+++ frontend/src/components/messageComponent.tsx
@@ -10,7 +10,7 @@ export const AnMessage: React.FC<messageComponentProps> = x => {
     const date = new Date(`${x.unit.params.date_published}`).toLocaleString("en-US", {timeZone: "Europe/Moscow"});
     return (
         <div className={styles.anotherMsg}>
-            <div className={styles.msgContent}>
+            <div className={styles.msgContentAnother}>
                 <div className={styles.msgAuthor}>{x.unit.owner}</div>
                 <div className={styles.msgText}>{x.unit.params.text}</div>
                 <div className={styles.msgDate}>{date}</div>
